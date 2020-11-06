130409650f316ebdd6d62ceba3fd0c70f76f9117
Sébastien Bouez <39953434+sebbouez@users.noreply.github.com>
2020-11-06 17:58:14 +0100

    Update ftp.ps1

--- Examples/PS1/ftp.ps1
+++ Examples/PS1/ftp.ps1
@@ -1,40 +1,37 @@
 param($artifacts, $ftp_uri, $ftp_user, $ftp_pass);
 
-function UploadToFtp($artifacts, $ftp_uri, $user, $pass)
-{
+function UploadToFtp($artifacts, $ftp_uri, $user, $pass) {
     $webclient = New-Object System.Net.WebClient 
     $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)  
     
-    Write-Host "starting ftp upload"
+    Write-Host "Starting ftp upload"
 
-    foreach($item in Get-ChildItem -recurse $artifacts)
-    { 
+    foreach ($item in Get-ChildItem -recurse $artifacts) { 
         $relpath = [system.io.path]::GetFullPath($item.FullName).SubString([system.io.path]::GetFullPath($artifacts).Length + 1)
 
-        if ($item.Attributes -eq "Directory")
-        {
-            try 
-            {
-                Write-Host "creating $($item.Name)"
+        if ((Get-ItemProperty $item.FullName).Mode[0] -eq "d") {
+                    
+            try {
+                Write-Host "Creating $($item.Name)"
                 $makeDirectory = [System.Net.WebRequest]::Create($ftp_uri+$relpath);
                 $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($user,$pass) 
                 $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
-                $makeDirectory.GetResponse();
-            
+                $makeDirectory.GetResponse();            
             }
             catch [Net.WebException] {
                 Write-Host "$($item.Name) probably already exists"
             }
 
             continue;
+        }    
+        else {     
+            Write-Host "Uploading $item"
+            $uri = New-Object System.Uri($ftp_uri + $relpath)
+            $webclient.UploadFile($uri, $item.FullName)
         }
-        
-        Write-Host "uploading $item"
-        $uri = New-Object System.Uri($ftp_uri+$relpath) 
-        $webclient.UploadFile($uri, $item.FullName)
-    }
 
-    Write-Host "Upload done." -foregroundcolor green
+        Write-Host "Upload done." -foregroundcolor green
+    }
 }
 
 UploadToFtp $artifacts $ftp_uri $ftp_user $ftp_pass
